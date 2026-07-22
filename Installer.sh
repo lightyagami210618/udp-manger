24b8fe3a1172b2d4f5cb3b40f58b5b83c0df2e45
lightyagami210618 <genpaypal03@gmail.com>
2026-07-23 05:04:51 +0700

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -32,8 +32,10 @@ mkdir -p /etc/zivpn
 cat <<EOF > /etc/zivpn/config.json
 {
   "listen": ":5667",
+  "protocol": "wechat-video",
   "cert": "/etc/zivpn/zivpn.crt",
   "key": "/etc/zivpn/zivpn.key",
+  "obfs": "df4d89ba09fb3c2865c696a8049f5c32",
   "auth": {
     "mode": "passwords",
     "config": []
